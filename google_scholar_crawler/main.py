71803ead5f71d93439ecb9914a4722d750d3fcb6
Jianyi Zhou <hitzhoujy@163.com>
2026-05-18 15:42:07 +0800

    update

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -3,8 +3,19 @@ import jsonpickle
 import json
 from datetime import datetime
 import os
+from urllib.parse import parse_qs, urlparse
 
-author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+def normalize_scholar_id(value: str) -> str:
+    value = value.strip()
+    if value.startswith('http://') or value.startswith('https://'):
+        query = parse_qs(urlparse(value).query)
+        value = query.get('user', [''])[0]
+    if not value:
+        raise ValueError('GOOGLE_SCHOLAR_ID must be a Google Scholar user id or profile URL')
+    return value
+
+google_scholar_id = normalize_scholar_id(os.environ['GOOGLE_SCHOLAR_ID'])
+author: dict = scholarly.search_author_id(google_scholar_id)
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
